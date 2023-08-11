bbba187c281efb623062749da6b996472585f0ef
Sambhav <samdkk9852@gmail.com>
2023-08-11 19:29:37 +0530

    About component is added and completed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import "./about.css"
+import ME from "../../assets/me-about.jpg"
+import { FaAward } from "react-icons/fa"
+import { FiUsers } from "react-icons/fi"
+import { VscFolderLibrary } from "react-icons/vsc"
 
 const About = () => {
   return (
     <section id='about'>
-      About
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="my profile" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>6 months </small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>1</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Project</h5>
+              <small>5</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Blanditiis reprehenderit deserunt ipsam aut libero quis, 
+            placeat pariatur voluptates quidem magni nulla eum cupiditate! 
+            Quis aliquam vitae aliquid ad accusantium, tenetur nulla impedit
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
     </section>
   )
 }
