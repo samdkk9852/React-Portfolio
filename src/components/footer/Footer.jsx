7779ecf035fbe4b35fcef7ed15c405faf01b5f41
Sambhav <samdkk9852@gmail.com>
2023-09-04 04:04:15 +0530

    Footer added along with the socials links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import "./footer.css"
+import {BsFacebook} from "react-icons/bs"
+import {BsInstagram} from "react-icons/bs"
+import {BsTwitter} from "react-icons/bs"
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <a href="#" className='footer__logo'>EGATOR</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services"Service></a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      
+      <div className="footer__socials">
+        <a href="https://facebook.com"><BsFacebook /></a>
+        <a href="https://instagram.com"><BsInstagram /></a>
+        <a href="https://twitter.com">< BsTwitter/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; EGATOR Tutorials. All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
